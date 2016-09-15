ffdaf693d1c703da443116e052c9d24f1a013fec
Nguyen Huu THo <huutho.snova@gmail.com>
2016-09-16 06:54:41 +0700

    te

---
+++ app/src/main/java/com/edu/gvn/jsoupdemo/fragment/online/hotmusic/KoreaHot.java
@@ -0,0 +1,30 @@
+package com.edu.gvn.jsoupdemo.fragment.online.hotmusic;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.edu.gvn.jsoupdemo.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class KoreaHot extends Fragment {
+
+
+    public KoreaHot() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_korea_hit, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/edu/gvn/jsoupdemo/fragment/online/hotmusic/UsUkHot.java
@@ -0,0 +1,32 @@
+package com.edu.gvn.jsoupdemo.fragment.online.hotmusic;
+
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.edu.gvn.jsoupdemo.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ *
+ */
+public class UsUkHot extends Fragment {
+
+
+    public UsUkHot() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_us_uk_hot, container, false);
+    }
+
+}
